821706b2a35abdddd5994bf86346af5d4b729d0d
jianglong <jl201621070337@gmail.com>
2017-09-10 06:19:04 -0700

    deteled

---
+++ linux/HomeWork/final_lib/googletest-release-1.8.0/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jl-broher/CodingWorld/Linux-Programming/linux/HomeWork/final_lib/googletest-release-1.8.0/googletest
+# Build directory: /home/jl-broher/CodingWorld/Linux-Programming/linux/HomeWork/final_lib/googletest-release-1.8.0/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
